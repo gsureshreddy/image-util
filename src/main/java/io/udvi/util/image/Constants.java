9145c8dd95e42c73c99584053e5075118bf4dea1
Suresh Reddy Guntaka <sureshreddy.guntaka@outlook.com>
2017-03-21 21:52:37 +0530

    test

---
+++ src/main/java/io/udvi/util/image/Constants.java
@@ -0,0 +1,6 @@
+package io.udvi.util.image;
+
+public class Constants {
+    public static final String IMAGE_WIDTH = "IMAGE_WIDTH";
+    public static final String IMAGE_HEIGHT = "IMAGE_HEIGHT";
+}
